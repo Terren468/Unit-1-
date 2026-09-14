855db51cc6f78a7848e1b97814fcac13e9c829ad
Terren468 <terrenh2@nycstudents.net>
2026-09-14 10:22:17 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,15 @@
-print("tests")
+import turtle
+from turtle import *
+t = Turtle()
+
+t.shape('turtle')
+
+
+def square(x):  
+    t. forward(x)
+    t.left (90)
+    t.forward(x)
+    t.left(90)
+    t.forward (x)
+
+turtle.done()
